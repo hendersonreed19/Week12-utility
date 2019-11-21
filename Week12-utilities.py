4af4af0c7cd7251cbe0eed3e0f11e5fb33400d5a
Reed Henderson <reedhenderson@csm-wl-dhcp-200-88.mines.edu>
2019-11-21 11:19:49 -0700

    Added file

---
+++ Week12-utilities.py
@@ -0,0 +1,25 @@
+#   YOUR GITHUB REPO 
+#   Reed Henderson
+#  ​ CSCI 102 – Section E
+#   Week 11 - Part B
+
+def PrintOutput(output):
+    print(output)
+def LoadFile(file):
+    lines = []
+    with open(file) as file1:
+        for line in file1:
+            lines.append(line)
+
+    PrintOutput(lines)
+    
+def UpdateString(string):
+    print()
+def FindWordCount(words):
+    print()
+def ScoreFinder(score):
+    print()
+def Union(Lista, Listb):
+    print()
+def intersection(Lista, Listb):
+    print()
